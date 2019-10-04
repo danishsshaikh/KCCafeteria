2f0ddb4068d6fcb0f13cc2f8074effa49820d258
Danish Shaikh <danishsshaikh@gmail.com>
2019-10-04 20:33:02 +0530

    some major changes

--- Files/index.php
+++ Files/index.php
@@ -1,5 +1,4 @@
 <?php 
-/* code by webdevtrick ( https://webdevtrick.com ) */
 session_start();
 $connect = mysqli_connect("localhost", "root", "", "cart");
 
@@ -33,6 +32,10 @@ if(isset($_POST["add_to_cart"]))
 			'item_quantity'		=>	$_POST["quantity"]
 		);
 		$_SESSION["shopping_cart"][0] = $item_array;
+        
+//        INSERT INTO tbl_order (itemname, itemquantity, itemprice, itemid)
+//values ('$i_name','$i_quantity','$i_price','$i_id')";
+        
 	}
 }
 
@@ -63,16 +66,78 @@ if(isset($_GET["action"]))
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     <link rel="stylesheet" type="text/css" href="shop.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
 </head>
 
 <body>
     <section>
-        <div id="main">
+        <div id="main" style="">
 
             <a href="index.php" id="main_name">KC Cafeteria</a>
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
+            &nbsp;
             <a href="about.html" id="mainn_nname">About us</a>
+            &nbsp;
+            &nbsp;
+
             <a href="faq.html" id="mainn_nname">FAQ</a>
-            <a href="Register.php" id="mainnn_nnname">Log Out</a>
+            &nbsp;
+            &nbsp;
+
+            <a href="logout.php" id="mainnn_nnname">Log Out</a>
 
         </div>
         <!--
@@ -98,9 +163,8 @@ if(isset($_GET["action"]))
     <br />
     <div class="container">
         <br />
-        <br />
-        <br />
-        <br /><br />
+
+
         <?php
 				$query = "SELECT * FROM tbl_product ORDER BY id ASC";
 				$result = mysqli_query($connect, $query);
@@ -109,9 +173,11 @@ if(isset($_GET["action"]))
 					while($row = mysqli_fetch_array($result))
 					{
 				?>
+        <!--        w:350 h:220-->
         <div class="col-md-4">
             <form method="post" action="index.php?action=add&id=<?php echo $row["id"]; ?>">
-                <div style="border:3px solid #5cb85c; background-color:whitesmoke; border-radius:2px; padding:16px;" align="center">
+                <div style="border:3px solid #5f0038; background-color:whitesmoke; border-radius:2px; padding:16px;" align="center">
+
                     <img src="images/<?php echo $row["image"]; ?>" class="img-responsive" style="width: 350px;height: 220px;" /><br />
 
                     <h4 class="text-info"><?php echo $row["name"]; ?></h4>
@@ -124,7 +190,7 @@ if(isset($_GET["action"]))
 
                     <input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>" />
 
-                    <input type="submit" name="add_to_cart" style="margin-top:5px;" class="btn btn-success" value="Add to Cart" />
+                    <input type="submit" name="add_to_cart" style="margin-top:5px; " class="btn btn-success" value="Add to Cart" />
 
                 </div>
             </form>
@@ -136,49 +202,54 @@ if(isset($_GET["action"]))
         <div style="clear:both"></div>
         <br />
         <h3>Order Details</h3>
-        <div class="table-responsive">
-            <table class="table table-bordered">
-                <tr>
-                    <th width="40%">Item Name</th>
-                    <th width="10%">Quantity</th>
-                    <th width="20%">Price</th>
-                    <th width="15%">Total</th>
-                    <th width="5%">Action</th>
-                </tr>
-                <?php
+        <form method="post" action="buyme.php">
+            <div class="table-responsive">
+                <table class="table table-bordered">
+                    <tr>
+                        <th width="40%">Item Name</th>
+                        <th width="10%">Quantity</th>
+                        <th width="20%">Price</th>
+                        <th width="15%">Total</th>
+                        <th width="5%">Action</th>
+                    </tr>
+                    <?php
 					if(!empty($_SESSION["shopping_cart"]))
 					{
 						$total = 0;
 						foreach($_SESSION["shopping_cart"] as $keys => $values)
 						{
 					?>
-                <tr>
-                    <td><?php echo $values["item_name"]; ?></td>
-                    <td><?php echo $values["item_quantity"]; ?></td>
-                    <td>₹ <?php echo $values["item_price"]; ?></td>
-                    <td>₹ <?php echo number_format($values["item_quantity"] * $values["item_price"], 2);?></td>
-                    <td><a href="index.php?action=delete&id=<?php echo $values["item_id"]; ?>"><span class="text-danger">Remove</span></a></td>
-                </tr>
-                <?php
+                    <tr>
+                        <td><?php echo $values["item_name"]; ?></td>
+                        <td><?php echo $values["item_quantity"]; ?></td>
+                        <td>₹ <?php echo $values["item_price"]; ?></td>
+                        <td>₹ <?php echo number_format($values["item_quantity"] * $values["item_price"], 2);?></td>
+                        <td><a href="index.php?action=delete&id=<?php echo $values["item_id"]; ?>"><span class="text-danger">Remove</span></a></td>
+                    </tr>
+                    <?php
 							$total = $total + ($values["item_quantity"] * $values["item_price"]);
 						}
 					?>
-                <tr>
-                    <td colspan="3" align="right">Total</td>
-                    <td align="right">₹ <?php echo number_format($total, 2); ?></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td colspan="5" align="right"> <input type="Submit" value="Submit" class="btn btn-primary">
-                    </td>
-                </tr>
-                <!--                    <input type="Submit" value="Submit" class="btn btn-primary">-->
+                    <tr>
+                        <td colspan="3" align="right">Total</td>
+                        <td align="right">₹ <?php echo number_format($total, 2); ?></td>
+                        <td></td>
+                    </tr>
+
+                    <tr>
+                        <td colspan="5" align="right"> <input type="Submit" value="Confirm" class="btn btn-primary">
+                        </td>
+                    </tr>
+        </form>
+        <!--                    <input type="Submit" value="Submit" class="btn btn-primary">-->
+        <!--
                 <?php
 					}
 					?>
+-->
 
-            </table>
-        </div>
+        </table>
+    </div>
     </div>
     </div>
     <br />
