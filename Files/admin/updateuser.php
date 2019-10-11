66a099fdd99c66b6388e443952e9aa27927864bc
Danish Shaikh <danishsshaikh@gmail.com>
2019-10-11 20:39:09 +0530

    updating menu php-sql

---
+++ Files/admin/updateuser.php
@@ -0,0 +1,29 @@
+<?php
+$connection = mysqli_connect("localhost","root","","cart");
+
+if(isset($_POST['updatebtn']))
+{
+    $n = $_POST['edit_name'];
+    $uu = $_POST['edit_uname'];
+    $e = $_POST['edit_email'];
+    $p = $_POST['edit_password'];
+
+    $query = "UPDATE registration SET name='$n', username = '$uu', email='$e', password = '$p' WHERE username = '$uu' ";
+    $query_run = mysqli_query($connection, $query);
+    
+    if($query_run)
+    {
+        $_SESSION['success'] = "Your Data is Updated";
+        header('Location: register.php'); 
+    }
+    else
+    {
+        $_SESSION['status'] = "Your Data is not Updated";
+        header('Location: register.php');
+    }
+    
+}
+
+
+
+?>
