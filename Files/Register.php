52a9cfe7f582292b38f4c376e30cda5800c3684d
Danish Shaikh <danishsshaikh@gmail.com>
2019-10-12 00:09:39 +0530

    <link rel

--- Files/Register.php
+++ Files/Register.php
@@ -8,6 +8,7 @@ include('login.php');
 <html>
 <head>
     <title>Registration / Login Page</title>
+    <link rel="icon" href="logo.png">
     <link rel="stylesheet" type="text/css" href="himanshu.css">
     <link rel="stylesheet"  type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 <!--    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">-->
