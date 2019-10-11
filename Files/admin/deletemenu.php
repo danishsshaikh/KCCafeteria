55e9848a0607ad7df092e3273739f5f930d995c4
Danish Shaikh <danishsshaikh@gmail.com>
2019-10-11 20:35:40 +0530

    delete menu php

---
+++ Files/admin/deletemenu.php
@@ -0,0 +1,26 @@
+<?php
+        $connection = mysqli_connect("localhost","root","","cart");
+
+if(isset($_POST['delete_btn']))
+{
+    $dn = $_POST['delete_name'];
+
+    $query = "DELETE FROM tbl_product WHERE name='$dn'";
+    $query_run = mysqli_query($connection, $query);
+    
+    if($query_run)
+    {
+        $_SESSION['success'] = "Your Data is Deleted";
+        header('Location: menu.php'); 
+    }
+    else
+    {
+        $_SESSION['status'] = "Your Data is not Deleted";
+        header('Location: menu.php');
+    }
+    
+}
+
+
+
+?>
